f7035a7cc3ff6ab695c186ddb02802cb65fb420d
Hayden <143210618+hayden-hurst@users.noreply.github.com>
2023-11-13 18:18:39 -0500

    Fixed navbar visibility and added route-specific navbar visibility

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes, Navigate, Outlet } from 'react-router-dom';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes, Navigate, Outlet} from 'react-router-dom';
 import Login from './components/auth/Login';
 import Signup from './components/auth/Signup';
 import Feed from './components/feed/Feed';
@@ -9,17 +10,35 @@ import DM from './components/messages/DirectMessages';
 import { useAuth } from './hooks/useAuth';
 
 function App() {
-    const { user } = useAuth();
+    // Use the useAuth hook to get the user's authentication status
+    const { user, isLoading } = useAuth();
+
+    // Define an array of routes where you want to display the Navbar
+    const navbarRoutes = ['/feed', '/home', '/profile', '/direct-messages'];
+
+    // State to track whether to display the Navbar
+    const [shouldDisplayNavbar, setShouldDisplayNavbar] = React.useState(false);
+
+    // useEffect to update the Navbar display when authentication status changes
+    React.useEffect(() => {
+        // Check if the user is authenticated and on a route where Navbar should be displayed
+        setShouldDisplayNavbar(user && navbarRoutes.includes(window.location.pathname));
+    }, [user, isLoading]);
+
+    if (isLoading) {
+        // You might want to show a loading indicator here
+        return <div>Loading...</div>;
+    }
 
     return (
         <Router>
-            {user && <Navbar />} {/* Render Navbar if the user is authenticated */}
+            {shouldDisplayNavbar ? <Navbar /> : null} {/* Render Navbar only on certain components */}
             <Routes>
                 <Route
                     path="/"
                     element={
                         user ? (
-                            // If authenticated, show the main content
+                            // If authenticated, show the main content using Outlet
                             <Outlet />
                         ) : (
                             // If not authenticated, redirect to the login page
@@ -27,18 +46,30 @@ function App() {
                         )
                     }
                 />
-                <Route path="/login" element={user ? <Navigate to="/home" replace /> : <Login />} />
-                <Route path="/signup" element={user ? <Navigate to="/home" replace /> : <Signup />} />
+                <Route
+                    path="/login"
+                    element={user ? <Navigate to="/home" replace /> : <Login />}
+                />
+                <Route
+                    path="/signup"
+                    element={user ? <Navigate to="/home" replace /> : <Signup />}
+                />
                 <Route
                     path="/profile"
                     element={user ? <Profile /> : <Navigate to="/login" replace />}
                 />
-                <Route path="/feed" element={user ? <Feed /> : <Navigate to="/login" replace />} />
+                <Route
+                    path="/feed"
+                    element={user ? <Feed /> : <Navigate to="/login" replace />}
+                />
                 <Route
                     path="/direct-messages"
                     element={user ? <DM /> : <Navigate to="/login" replace />}
                 />
-                <Route path="/home" element={user ? <Home /> : <Navigate to="/login" replace />} />
+                <Route
+                    path="/home"
+                    element={user ? <Home /> : <Navigate to="/login" replace />}
+                />
             </Routes>
         </Router>
     );

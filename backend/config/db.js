1ae541dc70fbd6509b4532846e577e31efafb77e
Hayden <143210618+hayden-hurst@users.noreply.github.com>
2023-12-11 03:02:42 -0500

    updated backend packages and setup db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,39 @@
-// Database configuration (e.g., MongoDB connection)
+import mongoose from 'mongoose';
+
+require('dotenv').config();
+
+const URI = process.env.MONGODB_URI;
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error(`MongoDB connection error: ${err}`);
+    });
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('MongoDB connection established');
+});
+
+db.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err}`);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB connection disconnected');
+});
+
+process.on('SIGINT', () => {
+    db.close(() => {
+        console.log('MongoDB connection closed through app termination');
+        process.exit(0);
+    });
+});
+
+export default mongoose;
